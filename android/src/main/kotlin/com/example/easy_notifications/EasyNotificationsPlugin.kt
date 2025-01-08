79e44567cef9c391693cc06eb0152f429aa66ef2
djungarik_DEV <145206969+djungarikDEV@users.noreply.github.com>
2025-01-08 21:00:50 +0100

    uploading

---
+++ android/src/main/kotlin/com/example/easy_notifications/EasyNotificationsPlugin.kt
@@ -0,0 +1,33 @@
+package com.example.easy_notifications
+
+import androidx.annotation.NonNull
+import io.flutter.embedding.engine.plugins.FlutterPlugin
+import io.flutter.plugin.common.MethodCall
+import io.flutter.plugin.common.MethodChannel
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler
+import io.flutter.plugin.common.MethodChannel.Result
+
+class EasyNotificationsPlugin: FlutterPlugin, MethodCallHandler {
+  private lateinit var channel : MethodChannel
+
+  override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+    channel = MethodChannel(flutterPluginBinding.binaryMessenger, "easy_notifications")
+    channel.setMethodCallHandler(this)
+  }
+
+  override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
+    when (call.method) {
+        "openApp" -> {
+            // Add implementation for opening app
+            result.success(null)
+        }
+        else -> {
+            result.notImplemented()
+        }
+    }
+  }
+
+  override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+    channel.setMethodCallHandler(null)
+  }
+}
